9761d15fa52f4eb3d5b310500159e4517021d4de
Matheus <mporto1994@gmail.com>
2022-02-22 22:40:25 -0300

    feat: Formulário struct and the api request has been made

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,45 @@
+import axios from "axios"
+import { useForm } from "react-hook-form"
 import { Container } from "../../components/Container"
 import { RegisterDivS } from "./styles"
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const Register = () => {
+    const schema = yup.object().shape({
+        name: yup.string().required("Nome obrigatório"),
+        email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+        password: yup.string().required("E-mail obrigatório"),
+        passwordConfirm: yup.string().required("E-mail obrigatório"),
+        course_module: yup.string().required("E-mail obrigatório")
+    })
+    const {register,handleSubmit} = useForm({
+        resolver:yupResolver(schema)
+    })
+
+    const applyRegister = (data) =>{
+        console.log(data)
+        axios.post('https://kenziehub.herokuapp.com/users',{
+            name:data.name,
+            contact:"something",
+            bio:"garbage",
+            password:data.password,
+            email:data.email,
+            course_module:data.course_module
+            }
+            .then((response)=>{
+                console.log(response)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+        )
+    }
+
+
+    
+
+
 
     return(
         <Container>
@@ -9,27 +47,27 @@ const Register = () => {
             <RegisterDivS>
                 <h2>Crie sua Conta</h2>
                 <span>Rápido e grátis, vamos nessa</span>
-                <form action="">
-                    <label htmlFor="">Nome
-                        <input type="text" />
+                <form onSubmit={handleSubmit(applyRegister)}>
+                    <label>Nome
+                        <input type="text" placeholder="Digite o Nome"    {...register("name")}/>
                     </label>
-                    <label htmlFor="">Email
-                        <input type="text" />
+                    <label>Email
+                        <input type="text" placeholder="Digite o Email"   {...register("email")}/>
                     </label>
-                    <label htmlFor="">Senha
-                        <input type="text" />
+                    <label>Senha
+                        <input type="text" placeholder="Digite a senha"   {...register("password")}/>
                     </label>
-                    <label htmlFor="">Confirmação de Senha
-                        <input type="text" />
+                    <label>Confirmação de Senha
+                        <input type="text" placeholder="Confirme a senha" {...register("passwordConfirm")}/>
                     </label>
-                    <label htmlFor="">Selecione o Módulo
-                        <select >
-                            <option value="">Primeiro Módulo</option>
-                            <option value="">Segundo Módulo</option>
-                            <option value="">Terceiro Módulo</option>
-                            <option value="">Quarto Módulo</option>
-                            <option value="">Quinto Módulo</option>
-                            <option value="">Sexto Módulo</option>
+                    <label >Selecione o Módulo
+                        <select  >
+                            <option value="Primeiro módulo (Introdução ao Frontend)" {...register("course_module")}>Primeiro Módulo</option>
+                            <option value="Segundo módulo (Frontend Avançado)"       {...register("course_module")}>Segundo Módulo</option>
+                            <option value="Terceiro módulo (Introdução ao Backend)"  {...register("course_module")}>Terceiro Módulo</option>
+                            <option value="Quarto módulo (Backend Avançado)"         {...register("course_module")}>Quarto Módulo</option>
+                            {/* <option value="">Quinto Módulo</option> */}
+                            {/* <option value="">Sexto Módulo</option> */}
                         </select>
                     </label>
                     <button type="submit">Cadastrar</button>
